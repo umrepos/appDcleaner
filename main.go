0f14c051b63800ea9e7d4211ccd93759fc1a3991
Medina <umedina@externosdeloittemx.com>
2021-08-09 15:54:22 -0500

    new main

--- main.go
+++ main.go
@@ -19,34 +19,35 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"log"
 	"net/http"
 	"os"
 )
 
 func main() {
-	// register hello function to handle all requests
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", hello)
+	log.SetFlags(log.Lshortfile)
 
-	// use PORT environment variable, or default to 8080
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "8080"
-	}
+	appVersion := fmt.Sprintf(os.Getenv("version"))
 
-	// start the web server on port and accept requests
-	log.Printf("Server listening on port %s", port)
-	log.Fatal(http.ListenAndServe(":"+port, mux))
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		tmpl, err := template.ParseFiles("index.html")
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+
+		data := struct {
+			AppVersion string
+		}{
+			AppVersion: appVersion,
+		}
+
+		err = tmpl.Execute(w, data)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+	})
+
+	//fmt.Println("http://127.0.0.1:3000")
+	//log.Fatalln(http.ListenAndServe("127.0.0.1:3000", nil))
 }
-
-// hello responds to the request with a plain-text "Hello, world" message.
-func hello(w http.ResponseWriter, r *http.Request) {
-	log.Printf("Serving request: %s", r.URL.Path)
-	host, _ := os.Hostname()
-	fmt.Fprintf(w, "Hello, world!\n")
-	fmt.Fprintf(w, "Version: 1.0.0\n")
-	fmt.Fprintf(w, "Hostname: %s\n", host)
-}
-
-// [END container_hello_app]
